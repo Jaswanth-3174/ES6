5b0a3681db3bc563f19cfb97f5135cdf2b036ae5
Jaswanth <s.jaswanth2004@gmail.com>
2025-05-17 19:01:16 +0530

    added

--- variables.js
+++ variables.js
@@ -1,3 +1,6 @@
+// let and const are introdced in ES6
+// var is function scoped, let and const are block scoped
+
 function interation(){
     for(var a = 0; a < 5; a++){
         console.log(a); 
@@ -7,7 +10,11 @@ function interation(){
     for(let b = 0; b < 5; b++){
         console.log(b); 
     }
-    console.log(b); // error as it is blocked scoped
+   // console.log(b); // error as it is blocked scoped
+
+    const c = 10;
+    console.log(c); // 10
+    // c = 20; // error as const cannot be reassigned
 }
 
 interation();
